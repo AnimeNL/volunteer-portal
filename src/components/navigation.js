11353266abad8a3fc330cb717f09ef69ecb2e5ed
Peter Beverloo <peter@lvp-media.com>
2017-06-22 00:17:21 +0100

    Separate out the menu to a component

---
+++ src/components/navigation.js
@@ -0,0 +1,12 @@
+// Copyright 2017 Peter Beverloo. All rights reserved.
+// Use of this source code is governed by the MIT license, a copy of which can
+// be found in the LICENSE file.
+
+// Driver for the Navigation component, which is the main menu for the volunteer portal. It compiles
+// the items that should be displayed in the menu, including links specific to the identified user
+// and the floors or areas in which events may be taking place.
+export class Navigation {
+    activate([convention, user]) {
+
+    }
+}

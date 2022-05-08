0cecd16af1460a0a6ac082be2c2031502c3d7332
Peter Beverloo <pbeverloo@gmail.com>
2022-05-08 19:00:18 +0100

    Display an error page when there are no active events

---
+++ src/schedule/components/LocationHeader.tsx
@@ -0,0 +1,55 @@
+// Copyright 2022 Peter Beverloo & AnimeCon. All rights reserved.
+// Use of this source code is governed by a MIT license that can be
+// found in the LICENSE file.
+
+import { h } from 'preact';
+
+import CardHeader from '@mui/material/CardHeader';
+import ReadMoreIcon from '@mui/icons-material/ReadMore';
+import { type SxProps, Theme } from '@mui/system';
+
+import { Link } from '../../Link';
+
+// CSS customizations applied to the <LocationHeader> and <LocationHeader> components.
+const kStyles: { [key: string]: SxProps<Theme> } = {
+    locationHeader: {
+        py: 1,
+
+        '& .MuiCardHeader-content': {
+            minWidth: 0,
+        },
+    },
+};
+
+// Properties passed to the <LocationHeader> component.
+interface LocationHeaderProps {
+    /**
+     * Text to display in the location header.
+     */
+    title: string;
+
+    /**
+     * URL of the page to navigate to when the header has been clicked on.
+     */
+    url: string;
+}
+
+// The <LocationHeader> component displays a <CardHeader> specific to an area or location. It's used
+// in various parts of the application, thus has been generalized.
+export function LocationHeader(props: LocationHeaderProps) {
+    const { title, url } = props;
+
+    return (
+        <Link href={url} sx={{ color: 'initial', textDecoration: 'initial' }}>
+            <CardHeader avatar={<ReadMoreIcon color="primary" />}
+                        sx={kStyles.locationHeader}
+                        title={title}
+                        titleTypographyProps={{
+                            color: 'primary',
+                            fontWeight: 'normal',
+                            noWrap: true,
+                            variant: 'h6'
+                        }} />
+        </Link>
+    );
+}
--- src/schedule/views/ActiveEventsView.tsx
+++ src/schedule/views/ActiveEventsView.tsx
@@ -1,18 +1,85 @@
-// Copyright 2021 Peter Beverloo & AnimeCon. All rights reserved.
+// Copyright 2022 Peter Beverloo & AnimeCon. All rights reserved.
 // Use of this source code is governed by a MIT license that can be
 // found in the LICENSE file.
 
 import { Fragment, h } from 'preact';
+import { useMemo, useState } from 'preact/hooks';
+
+import Alert from '@mui/material/Alert';
+import AlertTitle from '@mui/material/AlertTitle';
+import Card from '@mui/material/Card';
+import Stack from '@mui/material/Stack';
 
 import { AppTitle } from '../../AppTitle';
+import { DateTime } from '../../base/DateTime';
+import { Event, EventArea, EventSession } from '../../base/Event';
+import { LocationHeader } from '../components/LocationHeader';
 
-export function ActiveEventsView() {
+// Properties passed to the <ActiveEventsViews> component.
+interface ActiveEventsViewProps {
+    /**
+     * The event for which the active events should be listed.
+     */
+     event: Event;
+}
+
+// Lists the events that are currently active on the festival. A "card" will be displayed for each
+// area, immediately followed by the events currently active in that area.
+export function ActiveEventsView(props: ActiveEventsViewProps) {
+    const { event } = props;
+
+    const [ dateTime, setDateTime ] = useState(DateTime.local());
+    // TODO: Subscribe to an effect for propagating event schedule updates.
+
+    // Find all the active sessions. We work our way backwards to areas for this view as we're able
+    // to use the interval tree here, which is not the case for the location-specific views.
+    const sortedAreas = useMemo(() => {
+        const activeSessions = useMemo(() => event.findActiveSessions(dateTime), [ dateTime ]);
+        const activeAreas = new Map<EventArea, EventSession[]>();
+
+        // (1) Collate all the active sessions by the area they're being hosted in.
+        for (const session of activeSessions) {
+            if (!activeAreas.has(session.location.area))
+                activeAreas.set(session.location.area, [ session ]);
+            else
+                activeAreas.get(session.location.area)?.push(session);
+        }
+
+        // (2) Sort the areas by their name, in ascending order.
+        const sortedAreas = [ ...activeAreas.values() ].sort((lhs, rhs) =>
+            lhs[0].location.area.name.localeCompare(rhs[0].location.area.name));
+
+        // (3) Sort the list of sessions for each area.
+        for (let areaIndex = 0; areaIndex < sortedAreas.length; ++areaIndex) {
+            sortedAreas[areaIndex].sort((lhs, rhs) => {
+                // TODO: How to sort?
+                return 0;
+            });
+        }
+
+        return sortedAreas;
+
+    }, [ dateTime ]);
+
     return (
         <Fragment>
             <AppTitle title="Active events" />
-            <p>
-                ActiveEventsView
-            </p>
+            { !sortedAreas.length &&
+                <Fragment>
+                    <Alert elevation={1} severity="warning" sx={{ mt: { xs: 0, lg: 2 } }}>
+                        <AlertTitle>Nothing to see here…</AlertTitle>
+                        None of the <strong>{event.name}</strong> events are currently in progress.
+                        Has the festival started yet, or worse, has finished?
+                    </Alert>
+                    <Stack spacing={2} mt={2}>
+                        { [ ...event.areas() ].map(area => {
+                            const url = `/schedule/${event.identifier}/events/${area.identifier}/`;
+                            return <Card>
+                                       <LocationHeader title={area.name} url={url} />
+                                   </Card>;
+                        }) }
+                    </Stack>
+                </Fragment> }
         </Fragment>
     );
 }

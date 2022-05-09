d338d811c155d1b11e74fc818c285ffe8b8eaf38
Peter Beverloo <pbeverloo@gmail.com>
2022-05-09 22:49:50 +0100

    Interaction for the search function in the header

--- src/schedule/components/ApplicationBar.tsx
+++ src/schedule/components/ApplicationBar.tsx
@@ -1,4 +1,4 @@
-// Copyright 2021 Peter Beverloo & AnimeCon. All rights reserved.
+// Copyright 2022 Peter Beverloo & AnimeCon. All rights reserved.
 // Use of this source code is governed by a MIT license that can be
 // found in the LICENSE file.
 
@@ -7,12 +7,76 @@ import { h } from 'preact';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import AppBar from '@mui/material/AppBar';
 import IconButton from '@mui/material/IconButton';
+import InputBase from '@mui/material/InputBase';
 import SearchIcon from '@mui/icons-material/Search';
 import { SystemStyleObject, Theme } from '@mui/system';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import { alpha, styled } from '@mui/material/styles';
 
 import { kDesktopMenuWidthPx, kDesktopMaximumWidthPx } from '../ResponsiveConstants';
+
+// Containing element for the search field. Provides relative positioning, and a hover effect on
+// desktop to illustrate that interaction with this element is possible.
+// Based on https://mui.com/material-ui/react-app-bar/ (MIT)
+const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+}));
+
+// Containing element for the search icon, which should be visible both in the element's default
+// state, as well as in the expanded state where user input is accepted.
+// Based on https://mui.com/material-ui/react-app-bar/ (MIT)
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+    pointerEvents: 'none',
+    position: 'absolute',
+
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+
+    height: '100%',
+    padding: theme.spacing(0, 2),
+}));
+
+// Input element through which the user can initiate a search. Accepts user input, and expands over
+// the width of the parent <AppBar> when input is actually being accepted.
+// Based on https://mui.com/material-ui/react-app-bar/ (MIT)
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+
+    '& .MuiInputBase-input': {
+        borderRadius: theme.shape.borderRadius,
+        boxSizing: 'border-box',
+        cursor: 'pointer',
+        padding: theme.spacing(2, 0),
+        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+        transition: theme.transitions.create('width'),
+        width: theme.spacing(6),
+
+        '&:focus': {
+            backgroundColor: alpha(theme.palette.common.white, 0.2),
+            cursor: 'text',
+
+            // There's an additional 16 spacing of emptiness in the header: 2 on either side, 2
+            // between each of the three elements (4x2=8), and an additional 8 for the search box.
+            width: `calc(100vw - ${theme.spacing(16)})`,
+        },
+
+        '&:focus:hover': {
+            backgroundColor: alpha(theme.palette.common.white, 0.25),
+        }
+    },
+
+    [theme.breakpoints.up('md')]: {
+        '& .MuiInputBase-input': {
+            '&:focus': {
+                width: '50vw',
+            },
+        },
+    }
+}));
 
 // Styling used for the application bar. It's used for both the desktop view and for the mobile view
 // so there will be a fair amount of branching in this code, as the actual component will be kept
@@ -54,9 +118,13 @@ export function ApplicationBar(props: ApplicationBarProps) {
                 <Typography variant="h6" component="div" sx={kStyles.title}>
                     {props.title}
                 </Typography>
-                <IconButton size="large" color="inherit">
-                    <SearchIcon />
-                </IconButton>
+                <Search>
+                    <SearchIconWrapper>
+                        <SearchIcon />
+                    </SearchIconWrapper>
+                    <StyledInputBase placeholder="Search..."
+                                     inputProps={{ 'aria-label': 'search' }} />
+                </Search>
                 <IconButton size="large" color="inherit">
                     <AccountCircle />
                 </IconButton>

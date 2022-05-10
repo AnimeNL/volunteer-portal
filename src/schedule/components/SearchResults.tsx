256ad351342a9cbbe4ee539f9f156ced2a654d58
Peter Beverloo <pbeverloo@gmail.com>
2022-05-10 21:34:35 +0100

    Enable searching through the volunteers

--- src/schedule/components/SearchResults.tsx
+++ src/schedule/components/SearchResults.tsx
@@ -3,7 +3,15 @@
 // found in the LICENSE file.
 
 import { h } from 'preact';
+import { useMemo } from 'preact/compat';
+import { route } from 'preact-router';
 
+import Alert from '@mui/material/Alert';
+import Avatar from '@mui/material/Avatar';
+import List from '@mui/material/List';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
 import Popover from '@mui/material/Popover';
 import { SxProps, Theme } from '@mui/system';
 
@@ -18,6 +26,72 @@ const kStyles: { [key: string]: SxProps<Theme> } = {
         }
     })
 };
+
+// Structure describing an individual search result.
+export interface SearchResult {
+    /**
+     * Type of search result to display.
+     */
+    type: 'volunteer';
+
+    /**
+     * The avatar to display at the start of the search result, if any. When given, this should be
+     * a URL to the image resource that represents this result.
+     */
+    avatar?: string;
+
+    /**
+     * URL that the user should be routed to when they click on this search result. Required.
+     */
+    href: string;
+
+    /**
+     * Label that represents the search result. Required.
+     */
+    label: string;
+}
+
+// Searches the |event| for the given |query|, and returns the results, if any.
+//
+// TODO: How does search work?
+//
+// The returned array of search results will be sorted by priority, based on returning no more than
+// |limit| results. The |limit|ing is particularly useful for the inline search functionality.
+export function Search(event: Event, query: string, limit?: number): SearchResult[] {
+    const baseUrl = `/schedule/${event.identifier}`;
+
+    const normalizedQuery = query.trim().toLocaleLowerCase();
+    const results: SearchResult[] = [];
+
+    // Priority (1): Volunteers
+    for (const volunteer of event.volunteers()) {
+        if (!volunteer.name.toLocaleLowerCase().includes(normalizedQuery))
+            continue;
+
+        results.push({
+            type: 'volunteer',
+            avatar: volunteer.avatar,
+            href: `${baseUrl}/volunteers/${volunteer.identifier}/`,
+            label: volunteer.name,
+        });
+    }
+
+    // Priority (2): Areas
+    // TODO: Support areas
+
+    // Priority (3): Locations
+    // TODO: Support locations
+
+    // Priority (4): Events
+    // TODO: Support events
+
+    // TODO: Should we collapse certain locations and events? (I.e. Bag & Changing Rooms)
+    // TODO: Should we memorize historic search queries to optimize low-entropy searches?
+    // TODO: Should we give more weight to matches at the beginning of the string? ("Pe[ter]")
+
+    return (!limit || results.length <= limit) ? results
+                                               : results.splice(0, limit);
+}
 
 // Props accepted by the <SearchResults> component.
 export interface SearchResultsProps {
@@ -44,6 +118,19 @@ export interface SearchResultsProps {
 export function SearchResults(props: SearchResultsProps) {
     const { anchorEl, event, query } = props;
 
+    // The maximum number of search results to display in the inline search function.
+    const kInlineSearchResultLimit = 3;
+
+    // Memoize the search results based on the given |event| and |query|, because doing the actual
+    // search is a non-trivial traversal operation.
+    const searchResults = useMemo(() => {
+        if (!query)
+            return [ /* no results */ ];
+
+        return Search(event, query, kInlineSearchResultLimit);
+
+    }, [ event, query ]);
+
     return (
         <Popover PaperProps={{ sx: kStyles.container }}
                  anchorEl={anchorEl}
@@ -53,7 +140,35 @@ export function SearchResults(props: SearchResultsProps) {
                  elevation={4}
                  open={!!anchorEl && !!query}>
 
-            <b>{query}</b>
+            { !searchResults.length &&
+                <Alert severity="warning">
+                    No search results could be found.
+                </Alert> }
+
+            { searchResults.length > 0 &&
+                <List disablePadding>
+                    { searchResults.map(result => {
+                        // TODO: Display result avatars, including type-based default ones.
+                        // TODO: Make sure that search results have the ability to wrap.
+
+                        let avatar: h.JSX.Element | undefined;
+                        switch (result.type) {
+                            case 'volunteer':
+                                avatar = <Avatar alt={result.label}
+                                                 src={result.avatar} />
+                                break;
+                        }
+
+                        return (
+                            <ListItemButton>
+                                <ListItemAvatar>
+                                    {avatar}
+                                </ListItemAvatar>
+                                <ListItemText primary={result.label} />
+                            </ListItemButton>
+                        );
+                    }) }
+                </List> }
 
         </Popover>
     );
